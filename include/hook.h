c3f0fe3f70f767516f5ddee8bb9f6698b019a56f
Z3r0M3m0ry <liqitong@163.com>
2023-08-11 17:39:01 +0800

    Update hook.h

--- include/hook.h
+++ include/hook.h
@@ -17,7 +17,8 @@ public:
     virtual bool EnableEx(
         _In_ HANDLE hProcess, // handle to target process
         _In_ LPVOID target,   // target function
-        _In_ LPVOID detour,   // detour function, will be called instead of target after enabling hook
+        _In_ LPVOID detour,   // detour function, will be called instead of target after enabling hook. This value should be the address of the detour function in the remote process.
+                              // Make sure you have correctly injected the detour function into remote process, otherwise calling EnableEx would very likely crash the remote process.
         _Out_ LPVOID* origin = nullptr) noexcept = 0;
 
     virtual bool DisableEx() noexcept = 0;
